fdbe2d0d1824e84a68ba42db79542e7042c9fd45
Enenlingre <enenlingre@gmail.com>
2025-06-04 12:35:17 +0700

    feat: schedule last msg check

--- TelegramBot/__init__.py
+++ TelegramBot/__init__.py
@@ -3,7 +3,7 @@ from TelegramBot import config
 from aiogram import Bot, Dispatcher
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
-from TelegramBot.helpers.cron import scheduled_job
+from TelegramBot.helpers.cron import scheduled_job, check_last_message
 from pytz import timezone 
 
 bot_token=config.BOT_TOKEN
@@ -13,3 +13,4 @@ dp  = Dispatcher()
 
 scheduler = AsyncIOScheduler(timezone="Asia/Bangkok")
 scheduler.add_job(scheduled_job, CronTrigger(day_of_week='wed', hour=12, minute=0, timezone=timezone("Asia/Bangkok")), kwargs={"bot": bot}, )
+scheduler.add_job(check_last_message, CronTrigger(minute='*/2'), kwargs={"bot": bot})
